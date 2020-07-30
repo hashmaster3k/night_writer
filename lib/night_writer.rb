169fb429a9bd0afb533181beea26ad4bbdd9fb96
Hashim Gari <hashmaster3k@gmail.com>
2020-07-30 17:30:31 -0600

    Adds readable path attributes

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,8 +1,12 @@
 class NightWriter
 
-    def initialize(input_file, output_file)
-      puts "Created #{output_file} containing #{input_file.length} characters"
-    end #initialize
+  attr_reader :input_file, :output_file
+
+  def initialize(input_file, output_file)
+    @input_file  = input_file
+    @output_file = output_file
+    puts "Created #{output_file} containing #{input_file.length} characters"
+  end #initialize
 
 end #NightWriter
 
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -13,4 +13,13 @@ class NightWriterTest < Minitest::Test
     assert_instance_of NightWriter, nightwriter
   end
 
+  def test_has_readable_attributes
+    input_file  = './messages.txt'
+    output_file = './braille.txt'
+
+    nightwriter = NightWriter.new(input_file, output_file)
+
+    assert_equal input_file, nightwriter.input_file
+    assert_equal output_file, nightwriter.output_file
+  end
 end
