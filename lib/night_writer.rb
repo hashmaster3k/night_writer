6251d6a00f887bba9348ae5f910819d16ef2f795
Hashim Gari <hashmaster3k@gmail.com>
2020-07-31 12:26:14 -0600

    Adds encode english to braille method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,17 +1,45 @@
-class NightWriter
+require './lib/dictionary'
+require './lib/searchable'
 
-  attr_reader :input_file_path, :output_file_path
+class NightWriter
+  include Searchable
+
+  attr_reader :input_file_path, :output_file_path, :dictionary
 
   def initialize(input_file, output_file)
     @input_file_path  = "./#{input_file}"
     @output_file_path = "./#{output_file}"
-    puts "Created #{output_file} containing #{input_file.length} characters"
+    @dictionary = Dictionary.from_csv('./data/braille_dictionary.csv')
   end #initialize
 
-  def copy_text_and_write
-    copied_text = File.read(@input_file_path)
-    File.open(@output_file_path, "w") { |f| f.write copied_text}
+  def encode_to_braille
+    result = array_of_words_in_chars
+    3.times do
+      counter = 0
+      iteration = ""
+      result.each_with_index do |letter, i|
+        iteration.concat(letter[0] + letter[1])
+        result[i].slice!(0..1)
+        write_to_file(iteration) if counter == result.length - 1
+        counter += 1
+      end
+    end
   end
+
+  def write_to_file(text)
+    File.open(@output_file_path, 'a') {|file| file.puts text}
+  end #write_to_file
+
+  def char_array_from_file
+    File.read(@input_file_path).chomp.split("")
+  end #char_array_from_file
+
+  def array_of_words_in_chars
+    char_array_from_file.reduce([]) do |acc, character|
+      acc << find_matching_letter_braille_pair(character).braille.chars
+      acc
+    end
+  end #array_of_words_in_chars
 
 end #NightWriter
 
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -1,6 +1,7 @@
 require './lib/night_writer'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 
 class NightWriterTest < Minitest::Test
 
@@ -9,7 +10,6 @@ class NightWriterTest < Minitest::Test
     output_file = 'braille.txt'
 
     @nightwriter = NightWriter.new(input_file, output_file)
-
   end
 
   def test_it_exists
@@ -17,15 +17,52 @@ class NightWriterTest < Minitest::Test
   end
 
   def test_has_readable_attributes
-    assert_equal "./messages.txt", @nightwriter.input_file_path
-    assert_equal "./braille.txt",  @nightwriter.output_file_path
+    assert_equal './messages.txt', @nightwriter.input_file_path
+    assert_equal './braille.txt',  @nightwriter.output_file_path
+    assert_equal 27, @nightwriter.dictionary.length
   end
 
-  def test_copy_text_and_write
-    expected = File.read("./messages.txt")
-    @nightwriter.copy_text_and_write
+  def test_encodes_to_braille
+    File.delete('./braille.txt')
+    File.new('braille.txt', 'w')
 
-    assert_equal expected, File.read("./braille.txt")
+    before = File.read('./braille.txt')
+
+    assert_equal 0, before.length
+
+    @nightwriter.encode_to_braille
+
+    after = File.read('./braille.txt')
+
+    assert_equal 69, after.length
+  end
+
+  def test_write_to_file
+    File.delete('./braille.txt')
+    File.new('braille.txt', 'w')
+
+    before = File.read('./braille.txt')
+
+    assert_equal 0, before.length
+
+    @nightwriter.write_to_file('test')
+
+    after = File.read('./braille.txt')
+
+    assert_equal 5, after.length
+  end
+
+  def test_char_array_from_file
+    number = File.read('./messages.txt').chomp.length
+    assert_equal number, @nightwriter.char_array_from_file.length
+  end
+
+  def test_array_of_words_in_chars
+    @nightwriter.stubs(:char_array_from_file).returns(["y","o","y","o"])
+
+    expected = ["y","o","y","o"].length
+
+    assert_equal expected, @nightwriter.array_of_words_in_chars.length
   end
 
 end
