50b067836b29f10ccd8fd0a676c302c45590b477
Hashim Gari <hashmaster3k@gmail.com>
2020-07-31 15:16:45 -0600

    Creates night reader class and test file

---
+++ lib/night_reader.rb
@@ -0,0 +1,5 @@
+require './lib/dictionary'
+require './lib/searchable'
+
+class NightReader
+end
---
+++ test/night_reader_test.rb
@@ -0,0 +1,7 @@
+require './lib/night_reader'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+
+class NightReaderTest < Minitest::Test
+end
