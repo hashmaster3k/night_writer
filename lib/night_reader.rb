c469263e01e709568819f8173d8df065138256d1
Hashim Gari <hashmaster3k@gmail.com>
2020-07-31 15:20:10 -0600

    Adds readable attributes to night reader

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -4,10 +4,20 @@ require './lib/searchable'
 class NightReader
   include Searchable
 
+  attr_reader :input_file, :output_file, :dictionary
+
   def initialize(input_file, output_file)
     @input_file = input_file
     @output_file = output_file
     @dictionary = Dictionary.from_csv('./data/braille_dictionary.csv')
     #decode_from_braille
   end #initialize
+
+  def input_file_path
+    "./texts/#{@input_file}"
+  end
+
+  def output_file_path
+    "./texts/#{@output_file}"
+  end
 end
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -15,5 +15,13 @@ class NightReaderTest < Minitest::Test
   def test_it_exists
     assert_instance_of NightReader, @nightreader
   end
-  
+
+  def test_has_readable_attributes
+    assert_equal 'message.txt', @nightreader.input_file
+    assert_equal 'braille.txt', @nightreader.output_file
+    assert_equal './texts/message.txt', @nightreader.input_file_path
+    assert_equal './texts/braille.txt',  @nightreader.output_file_path
+    assert_equal 27, @nightreader.dictionary.length
+  end
+
 end
