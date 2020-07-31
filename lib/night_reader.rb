7ab968b7847c4bb4db96a5b08b00cec4b7cd1159
Hashim Gari <hashmaster3k@gmail.com>
2020-07-31 15:18:54 -0600

    Adds initialization of night reader class

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -2,4 +2,12 @@ require './lib/dictionary'
 require './lib/searchable'
 
 class NightReader
+  include Searchable
+
+  def initialize(input_file, output_file)
+    @input_file = input_file
+    @output_file = output_file
+    @dictionary = Dictionary.from_csv('./data/braille_dictionary.csv')
+    #decode_from_braille
+  end #initialize
 end
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -4,4 +4,16 @@ require 'minitest/pride'
 require 'mocha/minitest'
 
 class NightReaderTest < Minitest::Test
+
+  def setup
+    input_file  = 'message.txt'
+    output_file = 'braille.txt'
+
+    @nightreader = NightReader.new(input_file, output_file)
+  end
+
+  def test_it_exists
+    assert_instance_of NightReader, @nightreader
+  end
+  
 end
