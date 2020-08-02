2cf8a07eba4d7c13e0345c76ffc61ce0623a297f
Hashim Gari <hashmaster3k@gmail.com>
2020-08-02 07:53:52 -0600

    Moves over methods to module that used by both classes

--- lib/helpable.rb
+++ lib/helpable.rb
@@ -1,5 +1,17 @@
 module Helpable
 
+  def input_file_path
+    "./texts/#{input_file}"
+  end
+
+  def output_file_path
+    "./texts/#{output_file}"
+  end
+
+  def read_file
+    File.read(input_file_path)
+  end
+
   def find_matching_letter_braille_pair(letter)
     @dictionary.find {|letter_obj| letter_obj.letter == letter}
   end
@@ -8,9 +20,8 @@ module Helpable
     @dictionary.find {|letter_obj| letter_obj.braille == braille_letter}
   end
 
-  def created_message
-    content = File.read(output_file_path)
-    "Created #{output_file} containing #{content.length} characters"
+  def create_message
+    "Created #{output_file} containing #{read_file.length} characters"
   end
 
 end
--- test/helpable_test.rb
+++ test/helpable_test.rb
@@ -1,6 +1,7 @@
 require './lib/night_writer'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 
 
 class HelpableTest < Minitest::Test
@@ -12,6 +13,14 @@ class HelpableTest < Minitest::Test
     @nightwriter = NightWriter.new(input_file, output_file)
   end
 
+  def test_input_file_path
+    assert_equal './texts/message.txt', @nightwriter.input_file_path
+  end
+
+  def test_output_file_path
+    assert_equal './texts/braille.txt', @nightwriter.output_file_path
+  end
+
   def test_find_matching_letter
     assert_equal 'h', @nightwriter.find_matching_letter_braille_pair('h').letter
     assert_equal '0.00..', @nightwriter.find_matching_letter_braille_pair('h').braille
@@ -19,6 +28,15 @@ class HelpableTest < Minitest::Test
 
   def test_find_reverse_letter_braille_pair
     assert_equal 'h', @nightwriter.find_reverse_letter_braille_pair('0.00..').letter
+  end
+
+  def test_created_message
+    @nightwriter.stubs(:output_file).returns('greeting.txt')
+    @nightwriter.stubs(:read_file).returns('hello')
+
+    expected = "Created greeting.txt containing 5 characters"
+
+    assert_equal expected, @nightwriter.create_message
   end
 
 end
