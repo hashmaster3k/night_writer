92454c50e19ff7f97141a37f6cacf81e0896c1e8
Hashim Gari <hashmaster3k@gmail.com>
2020-08-03 15:07:53 -0600

    Combine similar methods and refactor code

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -4,26 +4,25 @@ require './lib/helpable'
 class NightReader
   include Helpable
 
-  attr_reader :input_file, :output_file, :dictionary, :decoded_message
+  attr_reader :input_file, :output_file, :dictionary
 
   def initialize(input_file, output_file)
     @input_file = input_file
     @output_file = output_file
-    @decoded_message = ""
     @dictionary = Dictionary.from_csv('./data/braille_dictionary.csv')
     decode_from_braille if File.exist?(input_file_path)
-  end #initialize
+  end
 
   def decode_from_braille
-    decode
-    write_to_file(decoded_message, output_file_path)
+    write_to_file(decode_message, output_file_path)
     puts create_message
   end
 
-  def decode
-    row_1 = create_row_1
-    row_2 = create_row_2
-    row_3 = create_row_3
+  def decode_message
+    decoded_message = ""
+    row_1 = create_rows[0]
+    row_2 = create_rows[1]
+    row_3 = create_rows[2]
     num_of_loops.times do
       message = ""
       message.concat row_1[0] + row_1[1]
@@ -38,59 +37,29 @@ class NightReader
   end
 
   def num_of_loops
-    create_row_1.length / 2
+    create_rows[0].length / 2
   end
 
   def split_file_content
     read_file(input_file_path).split("\n")
   end
 
-  def get_indices
-    num_list = (0..split_file_content.length - 1).to_a
-
-    r1 = []
-    num_list.each do |num|
-      r1 << num if num == 0 || num == 3 || num == 6 || num == 9 || num == 12
-    end
-
-    r2 = []
-    num_list.each do |num|
-      r2 << num if num == 1 || num == 4 || num == 7 || num == 10 || num == 13
-    end
-
-    r3 = []
-    num_list.each do |num|
-      r3 << num if num == 2 || num == 5 || num == 8 || num == 11 || num == 14
-    end
-
-    return r1, r2, r3
+  def indices_list
+    (0..split_file_content.length - 1).to_a
   end
 
-  def create_row_1
-    row = get_indices[0]
-    string = ""
-    row.each do |num|
-      string.concat split_file_content[num]
-    end
-    string
+  def indices_per_row
+    row1 = indices_list.find_all {|num| num if num == 0 || num == 3 || num == 6 || num == 9 || num == 12}
+    row2 = indices_list.find_all {|num| num if num == 1 || num == 4 || num == 7 || num == 10 || num == 13}
+    row3 = indices_list.find_all {|num| num if num == 2 || num == 5 || num == 8 || num == 11 || num == 14}
+    return row1, row2, row3
   end
 
-  def create_row_2
-    row = get_indices[1]
-    string = ""
-    row.each do |num|
-      string.concat split_file_content[num]
-    end
-    string
-  end
-
-  def create_row_3
-    row = get_indices[2]
-    string = ""
-    row.each do |num|
-      string.concat split_file_content[num]
-    end
-    string
+  def create_rows
+    row1 = indices_per_row[0].map {|num| split_file_content[num]}.join
+    row2 = indices_per_row[1].map {|num| split_file_content[num]}.join
+    row3 = indices_per_row[2].map {|num| split_file_content[num]}.join
+    return row1, row2, row3
   end
 
 end #NightReader
--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -20,10 +20,10 @@ class NightReaderTest < Minitest::Test
   def test_has_readable_attributes
     assert_equal 'braille.txt', @nightreader.input_file
     assert_equal 'original_message.txt', @nightreader.output_file
-    assert_equal 27, @nightreader.dictionary.length
+    assert_equal 32, @nightreader.dictionary.length
   end
 
-  def test_decode
+  def test_decode_message
     input_file  = 'nil_1.txt'
     output_file = 'nil_2.txt'
 
@@ -32,9 +32,8 @@ class NightReaderTest < Minitest::Test
     hi = "0..0\n000.\n...."
 
     nightreader.stubs(:read_file).returns(hi)
-    nightreader.decode
 
-    assert_equal "hi", nightreader.decoded_message
+    assert_equal "hi", nightreader.decode_message
   end
 
   def test_num_of_loops
@@ -52,10 +51,12 @@ class NightReaderTest < Minitest::Test
     assert_equal expected, @nightreader.split_file_content
   end
 
-  def test_get_indices
-    @nightreader.stubs(:num_list).returns([0,1,2,3,4,5,6,7,8])
-    expected = [[0,3,6], [1,4,7], [2,5,8]]
-    assert_equal expected, @nightreader.get_indices
+  def test_indices_per_row
+    @nightreader.stubs(:indices_list).returns([0,1,2,3,4,5,6,7,8])
+
+    assert_equal [0,3,6], @nightreader.indices_per_row[0]
+    assert_equal [1,4,7], @nightreader.indices_per_row[1]
+    assert_equal [2,5,8], @nightreader.indices_per_row[2]
   end
 
   def test_create_rows
@@ -64,9 +65,9 @@ class NightReaderTest < Minitest::Test
 
     hi = "0..0\n000.\n...."
     @nightreader.stubs(:read_file).returns(hi)
-    assert_equal "0..0", @nightreader.create_row_1
-    assert_equal "000.", @nightreader.create_row_2
-    assert_equal "....", @nightreader.create_row_3
+    assert_equal "0..0", @nightreader.create_rows[0]
+    assert_equal "000.", @nightreader.create_rows[1]
+    assert_equal "....", @nightreader.create_rows[2]
   end
 
 end
